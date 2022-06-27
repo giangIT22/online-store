a073fe8c7923ecb7376fea62c61c2c4fc52daf7a
Giang <giangIT22>
2022-06-27 17:15:57 +0700

    fix

--- resources/views/web/product/product_detail.blade.php
+++ resources/views/web/product/product_detail.blade.php
@@ -447,33 +447,27 @@
                                                 </div><!-- /.product-price -->
 
                                             </div><!-- /.product-info -->
-                                            <div class="cart clearfix animate-effect">
+                                            <div class="cart clearfix animate-effect fix-style">
                                                 <div class="action">
                                                     <ul class="list-unstyled">
                                                         <li class="add-cart-button btn-group">
-                                                            <button class="btn btn-primary icon" data-toggle="dropdown"
-                                                                type="button">
-                                                                <i class="fa fa-shopping-cart"></i>
-                                                            </button>
-                                                            <button class="btn btn-primary cart-btn" type="button">Add to
-                                                                cart</button>
-
+                                                            <a href="{{ route('product.detail', ['product_id' => $item->id, 'slug' => $item->product_slug]) }}"
+                                                                data-toggle="tooltip"
+                                                                class="btn btn-primary icon" type="button"
+                                                                data-original-title="" title=""> <i
+                                                                    class="fa fa-shopping-cart"></i>
+                                                                Tùy
+                                                                chọn</a>
                                                         </li>
-
-                                                        <li class="lnk wishlist">
-                                                            <a class="add-to-cart" href="detail.html" title="Wishlist">
-                                                                <i class="icon fa fa-heart"></i>
-                                                            </a>
-                                                        </li>
-
-                                                        <li class="lnk">
-                                                            <a class="add-to-cart" href="detail.html" title="Compare">
-                                                                <i class="fa fa-signal"></i>
-                                                            </a>
-                                                        </li>
+                                                        <li class="lnk"> <a data-toggle="tooltip"
+                                                                class="add-to-cart preview-product"
+                                                                id="11" data-original-title=""
+                                                                title="">
+                                                                <i class="fa fa-eye" aria-hidden="true"></i>
+                                                            </a> </li>
                                                     </ul>
-                                                </div><!-- /.action -->
-                                            </div><!-- /.cart -->
+                                                </div>
+                                            </div>
                                         </div><!-- /.product -->
 
                                     </div><!-- /.products -->
