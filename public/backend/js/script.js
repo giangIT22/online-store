4a79b53ce7b625cfd8c3d76090b23f40296e7ae1
giang.nguyentruong <giang22.fit@gmail.com>
2022-12-17 00:31:07 +0700

    fix

--- public/backend/js/script.js
+++ public/backend/js/script.js
@@ -12,7 +12,7 @@ $('.add-option').on('click', function() {
         var size = $(`#option-row-1 #list-color option:selected`).text();
         var color = $(`#option-row-1 #list-size option:selected`).text();
         var amount = $(`#option-row-1 #import_amount_1`).val();
-        var price = $(`#option-row-1 #import_price`).val();
+        var price = $(`#option-row-1 #import_price_1`).val();
 
         if (product != 'Chọn sản phẩm' &&
             size != 'Chọn màu sắc' &&
@@ -34,30 +34,61 @@ $('.add-option').on('click', function() {
             $(`#option-row-${i} #sum_price_1`).attr('id', `sum_price_${i}`);
         }
     } else if (i > 1) {
-        var product = $(`#option-row-${i} #list-product option:selected`).text();
-        var size = $(`#option-row-${i} #list-color option:selected`).text();
-        var color = $(`#option-row-${i} #list-size option:selected`).text();
-        var amount = $(`#option-row-${i} #import_amount_${i}`).val();
-        var price = $(`#option-row-${i} #import_price`).val();
+        let element = document.querySelector(`#option-row-${i}`);
+        if (element !== null) {
+            var product = $(`#option-row-${i} #list-product option:selected`).text();
+            var size = $(`#option-row-${i} #list-color option:selected`).text();
+            var color = $(`#option-row-${i} #list-size option:selected`).text();
+            var amount = $(`#option-row-${i} #import_amount_${i}`).val();
+            var price = $(`#option-row-${i} #import_price_${i}`).val();
 
-        if (product != 'Chọn sản phẩm' &&
-            size != 'Chọn màu sắc' &&
-            color != 'Chọn kích thước' &&
-            amount != '' && price != ''
-        ) {
-            ++i;
-            itemOption = `<tr id="option-row-${i}">${document.querySelector('#option-row-1').innerHTML}</tr>`;
-            $('.choose-option').append(itemOption);
-            $(`#option-row-${i}`).append(`<td>
-                <a href="#" class="remove-first">
-                    <i class="fa fa-trash"></i></a>
-            </td>`);
-            document.querySelector(`#option-row-${i}`).lastElementChild.querySelector('a').setAttribute('id', `remove-option-${i}`);
-            document.querySelector(`#option-row-${i}`).lastElementChild.querySelector('a').setAttribute('class', `remove-option`);
-            document.querySelector(`#option-row-${i}`).lastElementChild.querySelector('a').setAttribute('onclick', `removeOption('#option-row-${i}')`);
-            $(`#option-row-${i} #import_amount_1`).attr('id', `import_amount_${i}`);
-            $(`#option-row-${i} #import_price_1`).attr('id', `import_price_${i}`);
-            $(`#option-row-${i} #sum_price_1`).attr('id', `sum_price_${i}`);
+            if (product != 'Chọn sản phẩm' &&
+                size != 'Chọn màu sắc' &&
+                color != 'Chọn kích thước' &&
+                amount != '' && price != ''
+            ) {
+                ++i;
+                itemOption = `<tr id="option-row-${i}">${document.querySelector('#option-row-1').innerHTML}</tr>`;
+                $('.choose-option').append(itemOption);
+                $(`#option-row-${i}`).append(`<td>
+                    <a href="#" class="remove-first">
+                        <i class="fa fa-trash"></i></a>
+                </td>`);
+                document.querySelector(`#option-row-${i}`).lastElementChild.querySelector('a').setAttribute('id', `remove-option-${i}`);
+                document.querySelector(`#option-row-${i}`).lastElementChild.querySelector('a').setAttribute('class', `remove-option`);
+                document.querySelector(`#option-row-${i}`).lastElementChild.querySelector('a').setAttribute('onclick', `removeOption('#option-row-${i}')`);
+                $(`#option-row-${i} #import_amount_1`).attr('id', `import_amount_${i}`);
+                $(`#option-row-${i} #import_price_1`).attr('id', `import_price_${i}`);
+                $(`#option-row-${i} #sum_price_1`).attr('id', `sum_price_${i}`);
+            }
+        } else {
+            --i;
+            var product = $(`#option-row-${i} #list-product option:selected`).text();
+            var size = $(`#option-row-${i} #list-color option:selected`).text();
+            var color = $(`#option-row-${i} #list-size option:selected`).text();
+            var amount = $(`#option-row-${i} #import_amount_${i}`).val();
+            var price = $(`#option-row-${i} #import_price_${i}`).val();
+            console.log('abc')
+            if (product != 'Chọn sản phẩm' &&
+                size != 'Chọn màu sắc' &&
+                color != 'Chọn kích thước' &&
+                amount != '' && price != ''
+            ) {
+                ++i;
+                console.log('gianmg');
+                itemOption = `<tr id="option-row-${i}">${document.querySelector('#option-row-1').innerHTML}</tr>`;
+                $('.choose-option').append(itemOption);
+                $(`#option-row-${i}`).append(`<td>
+                    <a href="#" class="remove-first">
+                        <i class="fa fa-trash"></i></a>
+                </td>`);
+                document.querySelector(`#option-row-${i}`).lastElementChild.querySelector('a').setAttribute('id', `remove-option-${i}`);
+                document.querySelector(`#option-row-${i}`).lastElementChild.querySelector('a').setAttribute('class', `remove-option`);
+                document.querySelector(`#option-row-${i}`).lastElementChild.querySelector('a').setAttribute('onclick', `removeOption('#option-row-${i}')`);
+                $(`#option-row-${i} #import_amount_1`).attr('id', `import_amount_${i}`);
+                $(`#option-row-${i} #import_price_1`).attr('id', `import_price_${i}`);
+                $(`#option-row-${i} #sum_price_1`).attr('id', `sum_price_${i}`);
+            }
         }
     }
 });
@@ -70,7 +101,6 @@ function removeOption(id) {
 function getAmount() {
     let id = event.target.id;
     let index = id.slice(id.lastIndexOf('_') + 1);
-    console.log(index);
     let amount = parseInt($('#import_amount_' + index).val());
     let price = parseInt($('#import_price_' + index).val());
     if (!price) {
